4bd2910c160acdf1a3b5f91c8fc46ff39d01e1a5
Alain Buisseret <me@alain.dev>
2024-11-18 12:27:19 +0100

    Apply a min constraint (much better vsiual)

--- src/view/tui.rs
+++ src/view/tui.rs
@@ -70,7 +70,7 @@ impl<'g, 'ui> Widget for View<'g, 'ui> {
             .constraints(vec![
                 Constraint::Fill(1),
                 Constraint::Fill(1),
-                Constraint::Percentage(60),
+                Constraint::Min(40),
                 Constraint::Fill(1),
                 Constraint::Fill(1),
             ])
